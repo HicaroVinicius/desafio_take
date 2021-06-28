a127204f04c1ccdae132bd467aa33b8970fd24be
HicaroVinicius <hicaro_vinicius@hotmail.com>
2021-06-28 17:59:36 -0300

    Update repositorios.js

--- src/controllers/repositorios.js
+++ src/controllers/repositorios.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function getReps() {
   try {
-    const response = await axios.get('https://api.github.com/users/takenet/repos?q=&type=&language=c%23&sort=created_at:asc&per_page=5');
+    const response = await axios.get('https://api.github.com/users/takenet/repos?q=&type=public&sort=created&direction=asc&per_page=50');
     if(response && response.data && response.data.length > 0){
         return response.data;
     }
@@ -16,19 +16,31 @@ exports.getRepositorios = async (_req, res) => {
   try {
     const reps = await getReps();
     if(reps){
-        const response = reps.map((rep)=>{
-            return {
-                    header: {
-                        type: "application/vnd.lime.media-link+json",
-                        value: {
-                            title: rep.name,
-                            text: rep.description,
-                            type: "image/jpeg",
-                            uri: rep.owner.avatar_url
+        let cont = 0;
+        const response = [];
+        // eslint-disable-next-line no-restricted-syntax
+        for (const rep of reps) {
+            if(cont < 5){
+                if(rep.language && rep.language === "C#"){
+                    // eslint-disable-next-line no-plusplus
+                    cont++;
+                    const aux = {
+                        header: {
+                            type: "application/vnd.lime.media-link+json",
+                            value: {
+                                title: rep.name,
+                                text: rep.description,
+                                type: "image/jpeg",
+                                uri: rep.owner.avatar_url
+                            }
                         }
-                    }
-                }
-        });
+                    };
+                    response.push(aux);
+                } 
+            }else{
+                break;
+            }
+        }
         return res.status(200).send({
             itemType: "application/vnd.lime.document-select+json",
             items: response
