f40ba0c176a1846fe2d1390c76d8c5655737b101
HicaroVinicius <hicaro_vinicius@hotmail.com>
2021-08-27 10:21:12 -0300

    Update repositorios.js

--- src/controllers/repositorios.js
+++ src/controllers/repositorios.js
@@ -6,9 +6,18 @@ const axios = require('axios');
 async function getReps() {
   try {
     const response = await axios.get(`https://api-content.ingresso.com/v0/sessions/city/2/theater/14/partnership/ancar_novaamerica`);
-    console.log(response.data)
     if (response && response.data && response.data.length > 0) {
-      return [response.data[0], response.data[1], response.data[2]];
+      const resp = response.data.slice(0, 3);
+      const out = resp.map(item => {
+        return {
+          dateFormatted: item.dateFormatted,
+          "date": item.date,
+          "dayOfWeek": item.dayOfWeek,
+          "isToday": item.isToday
+        }
+      });
+      return out;
+      // return [response.data[0].movies[0].rooms[0].sessions[0].price, response.data[1].movies[0].rooms[0].sessions[0].price, response.data[2].movies[0].rooms[0].sessions[0].price];
     }
     return [];
   } catch (error) {
@@ -20,8 +29,6 @@ exports.getRepositorios = async (_req, res) => {
   try {
     // Função recursiva que enquanto não recuperar os 5 primeiros repositórios em C# segue realizando buscas nas páginas seguintes.
     const reps = await getReps();
-    console.log(reps)
-    console.log(reps.length)
     if (reps.length > 0) {
       return res.status(200).send(reps);
     }
