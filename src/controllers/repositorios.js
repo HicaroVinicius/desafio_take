a54753e3d010ada8276b2494ae3c7dbd6cc0a1af
HicaroVinicius <hicaro_vinicius@hotmail.com>
2021-08-27 09:55:49 -0300

    Update repositorios.js

--- src/controllers/repositorios.js
+++ src/controllers/repositorios.js
@@ -1,22 +1,15 @@
 const axios = require('axios');
 
-const LANG = 'C#';
-const USER = 'takenet';
+// const LANG = 'C#';
+// const USER = 'takenet';
 
-async function getReps(page, respBefore = []) {
+async function getReps() {
   try {
-    const response = await axios.get(`https://api.github.com/users/${USER}/repos?q=&type=public&sort=created&direction=asc&page=${page}`);
-    if(response && response.data && response.data.length > 0){
-      // Adiciona a resposta da requisição ao array anterior, filtrando apenas os repositórios com linguagem especificada
-      const respCShap = respBefore.concat(response.data.filter((i)=>{return i.language === LANG}));
-      if(respCShap.length > 4){
-        // Se houver mais 5 ou mais repositórios, retorna com a resposta
-        return respCShap;
-      }
-      // Enquanto não houver 5 ou mais repositórios, busca nas páginas seguintes até que não existam mais respostas.
-      return await getReps(page + 1, respCShap);
+    const response = await axios.get(`https://api-content.ingresso.com/v0/sessions/city/2/theater/14/partnership/ancar_novaamerica`);
+    if (response && response.data && response.data.length > 0) {
+      return response.data;
     }
-    return [];  
+    return [];
   } catch (error) {
     return [];
   }
@@ -25,31 +18,13 @@ async function getReps(page, respBefore = []) {
 exports.getRepositorios = async (_req, res) => {
   try {
     // Função recursiva que enquanto não recuperar os 5 primeiros repositórios em C# segue realizando buscas nas páginas seguintes.
-    let reps = await getReps(1);
-    if(reps.length > 0){
-      // Trata para exibir apenas os 5 primeiros
-      reps = reps.slice(0, 5);
-      const response = reps.map((rep)=>{
-        return {
-          header: {
-            type: "application/vnd.lime.media-link+json",
-            value: {
-                title: rep.name,
-                text: rep.description,
-                type: "image/jpeg",
-                uri: rep.owner.avatar_url
-            }
-          }
-        }
-      });
-        return res.status(200).send({
-            itemType: "application/vnd.lime.document-select+json",
-            items: response
-        });
+    const reps = await getReps();
+    if (reps.length > 0) {
+      return res.status(200).send(reps);
     }
-    return res.status(406).send({msg: 'Nenhum repositório encontrado'});
+    return res.status(406).send({ msg: 'Nenhum repositório encontrado' });
   } catch (error) {
-    return res.status(500).send({msg: 'Serviço indisponível no momento'});
+    return res.status(500).send({ msg: 'Serviço indisponível no momento' });
   }
 };
 
